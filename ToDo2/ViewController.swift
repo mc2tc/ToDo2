dbe8c35a8b0c7e468a9297749381143f490250f6
mclifford <mclifford@sittercity.com>
2016-11-20 13:35:22 -0500

    Initial Commit

---
+++ ToDo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo2
+//
+//  Created by Martin Clifford on 11/20/16.
+//  Copyright © 2016 Martin Clifford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
